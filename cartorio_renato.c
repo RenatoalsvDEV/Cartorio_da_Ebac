3916cee24e33c9e322b99b8cf75e72b9aa651ff4
RenatoalsvDEV <160912000+RenatoalsvDEV@users.noreply.github.com>
2024-02-22 14:39:04 -0300

    Opção de saida
    Inclui no sistema a opção de saída para o usuario

--- cartorio_renato.c
+++ cartorio_renato.c
@@ -129,6 +129,7 @@ int main()
 	    printf("\t1 - Registrar nomes\n");
 	    printf("\t2 - Consultar nomes\n");
 	    printf("\t3 - Deletar nomes\n\n"); 
+	    printf("\t4 - Sair do sistema\n\n");
 	    printf("\t    opção:"); // fim do menu
 
 	    scanf("%d", &opcao); //armazenando a escolha do usuario
@@ -149,6 +150,11 @@ int main()
 			deletar();
 		    break;
 		    
+		    case 4:
+	        printf("Obrigado por utilizar o sistema!\n");
+	        return 0;
+	        break;
+	        
 		    default:
 			printf("essa opção nao esta disponivel!\n"); 
    		    system("pause");	
